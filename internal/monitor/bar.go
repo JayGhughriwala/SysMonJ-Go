238469d19cf61bbe2a3b600d9a110845a6a9ea26
Jay <jayghughri1@gmail.com>
2025-04-05 17:36:21 +0530

    test2

--- internal/monitor/bar.go
+++ internal/monitor/bar.go
@@ -4,6 +4,9 @@ import "strings"
 
 func GetBar(percent float64) string {
 	total := 20
-	filler := int((percent / 100.0) * float64(total))
-	return "[" + strings.Repea
+	filled := int((percent / 100.0) * float64(total))
+	return "[" + strings.Repeat("█", filled) + strings.Repeat("░", total-filled) + "]"
+
 }
+
+// █  ░
