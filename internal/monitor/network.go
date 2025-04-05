be63cb7f172c046d3c69adbf81ca0a4e93bcd267
Jay <jayghughri1@gmail.com>
2025-04-05 17:41:51 +0530

    testig3

--- internal/monitor/network.go
+++ internal/monitor/network.go
@@ -7,6 +7,8 @@ import (
 )
 
 func GetNetworkStats() string {
+	// here false important because false value gives all combine values
+
 	io, _ := net.IOCounters(false)
 	stats := io[0]
 	return fmt.Sprintf("↑ %.2f MB   ↓ %.2f MB", float64(stats.BytesSent)/1e6, float64(stats.BytesRecv)/1e6)
